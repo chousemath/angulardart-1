6290981ecf932733af7b153a469058c8588afeb4
Joseph Sungpil Choi <chousemath@gmail.com>
2018-07-25 18:51:51 +0900

    Tried using a dart class

--- lib/src/todo_list/todo_list_component.dart
+++ lib/src/todo_list/todo_list_component.dart
@@ -5,6 +5,12 @@ import 'package:angular_components/angular_components.dart';
 
 import 'todo_list_service.dart';
 
+class Item {
+  String title;
+  String description;
+  Item(this.title, this.description);
+}
+
 @Component(
   selector: 'todo-list',
   styleUrls: ['todo_list_component.css'],
@@ -22,28 +28,31 @@ import 'todo_list_service.dart';
 class TodoListComponent implements OnInit {
   final TodoListService todoListService;
 
-  List<String> items = [];
-  List<String> trashcan = [];
-  String newTodo = '';
+  List<Item> items = [];
+  List<Item> trashcan = [];
+  String title = '';
+  String description = '';
 
   TodoListComponent(this.todoListService);
 
   @override
   Future<Null> ngOnInit() async {
-    items = await todoListService.getTodoList();
+    // items = await todoListService.getTodoList();
   }
 
   void add() {
+    if (title == '' || description == '') return;
     print('I was clicked!');
-    items.add(newTodo);
-    newTodo = '';
+    items.add(new Item(title, description));
+    title = '';
+    description = '';
   }
 
-  String remove(int index) {
-    String removedItem = items.removeAt(index);
+  Item remove(int index) {
+    Item removedItem = items.removeAt(index);
     trashcan.add(removedItem);
     return removedItem;
   }
 
-  String removePermanent(int index) => trashcan.removeAt(index);
+  Item removePermanent(int index) => trashcan.removeAt(index);
 }
